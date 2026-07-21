bbf37725a80062a32aa80896eec2998e6b4fb8ea
martin <martin.queval04@gmail.com>
2026-07-21 11:37:01 +0200

    fix(SCRUM-232): adapte les 6 fixtures AppState restantes au champ regles_categorisation

--- tests/scrum247_rattachement_banque.rs
+++ tests/scrum247_rattachement_banque.rs
@@ -20,6 +20,7 @@ use ch_api_budgy::repository::bank_accounts::SqlxBankAccountsWriteAdapter;
 use ch_api_budgy::repository::bank_transactions::SqlxBankTransactionsWriteAdapter;
 use ch_api_budgy::repository::categories::SqlxCategoriesRepository;
 use ch_api_budgy::repository::consents::SqlxConsentsWriteAdapter;
+use ch_api_budgy::repository::regles_categorisation::SqlxReglesCategorisationRepository;
 use ch_api_budgy::routes::router;
 use ch_api_budgy::services::jwt::JwtService;
 use ch_api_budgy::state::AppState;
@@ -144,6 +145,7 @@ fn state_avec_source(db: &DisposableDb, bank_source: Arc<dyn BankDataSource>) ->
             crypto.clone(),
         )),
         categories: Arc::new(SqlxCategoriesRepository::new(db.pool.clone())),
+        regles_categorisation: Arc::new(SqlxReglesCategorisationRepository::new(db.pool.clone())),
         bank_accounts: Arc::new(SqlxBankAccountsWriteAdapter::new(
             db.pool.clone(),
             crypto.clone(),
